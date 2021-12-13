56f65641148d656c6811478cd56638ab0e24388d
sekainoowaka <53969843+sekainoowaka@users.noreply.github.com>
2021-12-14 00:50:05 +0800

    Add files via upload

--- call.cpp
+++ call.cpp
@@ -11,8 +11,9 @@ struct Event{
     string name;
     Status stat;
     int duration;
-    bool is_calling = false;
-    bool is_answered = false;
+    int count = 0;
+//    bool call = false;
+//    bool answer = false;
 };
 
 int main() {
@@ -31,11 +32,11 @@ int main() {
     Dlist<Event> allMbr;
     Dlist<Event> allMbr_tmp;
 
-    int pCount = 0;
-    int gCount = 0;
-    int sCount = 0;
-    int rCount = 0;
-    bool is_answering = false;
+//    int pCount = 0;
+//    int gCount = 0;
+//    int sCount = 0;
+//    int rCount = 0;
+    bool occupied = false;
 
     ////read file input, and insert into call members
     for (int i = 0; i < N; i++) {
@@ -92,13 +93,15 @@ int main() {
 
         while (!platinumMbr.isEmpty()) {
             Event *pTmp = platinumMbr.removeFront();
-            if (pCount > 0 && pCount < pTmp->duration) {
-                pCount++;
+            if (pTmp->count > 0 && pTmp->count < pTmp->duration) {
+                pTmp->count++;
+                // if we use pCount, there will be double counting problems:
+                //
                 platinumMbr_tmp.insertBack(pTmp);
-            } else if (pCount == pTmp->duration) {
-                pCount = 0;
-                is_answering = false;
-                delete pTmp; // ?
+            } else if (pTmp->count == pTmp->duration) {
+//                pCount = 0;
+                occupied = false;
+                delete pTmp;
             } else platinumMbr_tmp.insertBack(pTmp);
         }
         platinumMbr = platinumMbr_tmp;
@@ -113,19 +116,20 @@ int main() {
 //                platinumMbr.insertFront(pTmp);
 //            } else if (pCount == pTmp->duration) {
 //                pCount = 0;
-//                is_answering = false;
+//                occupied = false;
 //                delete pTmp;
 //            }
-//        } // makes this not a queue?
+//        } // makes not a queue?
 
         while (!goldMbr.isEmpty()) {
             Event *gTmp = goldMbr.removeFront();
-            if (gCount > 0 && gCount < gTmp->duration) {
-                gCount++;
+            if (gTmp->count > 0 && gTmp->count < gTmp->duration) {
+                // first answer count as 1, second tick count as 2, no problem
+                gTmp->count++;
                 goldMbr_tmp.insertBack(gTmp);
-            } else if (gCount == gTmp->duration) {
-                gCount = 0;
-                is_answering = false;
+            } else if (gTmp->count == gTmp->duration) {
+//                gCount = 0;
+                occupied = false;
                 delete gTmp;
             } else goldMbr_tmp.insertBack(gTmp);
         }
@@ -136,12 +140,12 @@ int main() {
 
         while (!silverMbr.isEmpty()) {
             Event *sTmp = silverMbr.removeFront();
-            if (sCount > 0 && sCount < sTmp->duration) {
-                sCount++;
+            if (sTmp->count > 0 && sTmp->count < sTmp->duration) {
+                sTmp->count++;
                 silverMbr_tmp.insertBack(sTmp);
-            } else if (sCount == sTmp->duration) {
-                sCount = 0;
-                is_answering = false;
+            } else if (sTmp->count == sTmp->duration) {
+//                sCount = 0;
+                occupied = false;
                 delete sTmp;
             } else silverMbr_tmp.insertBack(sTmp);
         }
@@ -152,12 +156,12 @@ int main() {
 
         while (!regularMbr.isEmpty()) {
             Event *rTmp = regularMbr.removeFront();
-            if (rCount > 0 && rCount < rTmp->duration) {
-                rCount++;
+            if (rTmp->count > 0 && rTmp->count < rTmp->duration) {
+                rTmp->count++;
                 regularMbr_tmp.insertBack(rTmp);
-            } else if (rCount == rTmp->duration) {
-                rCount = 0;
-                is_answering = false;
+            } else if (rTmp->count == rTmp->duration) {
+//                rCount = 0;
+                occupied = false;
                 delete rTmp;
             } else regularMbr_tmp.insertBack(rTmp);
         }
@@ -166,38 +170,100 @@ int main() {
             delete regularMbr_tmp.removeFront();
         }
 
-        // in !is_answering: if count == 0, count++
+        // in !occupied: if count == 0, count++
         // outside: if count > 0, continue to count++
-        // until count = duration, reset count as 0, is_answering = false, destroy in waitList & member list
+        // until count = duration, reset count as 0, occupied = false, destroy in waitList & member list
 
-        if (!is_answering) { // once the call is answered, we can never go into this argument
-            if (pCount == 0 && !platinumMbr.isEmpty()) { // think carefully: what if there are multiple platinum members?
-                pCount++;
-                is_answering = true;
-                Event *pTmp = platinumMbr.removeFront();
-                cout << "Answering call from " << pTmp->name << endl;
-                platinumMbr.insertFront(pTmp);
+        if (!occupied) { // once the call is answered, we can never go into this argument
+//            if (pCount == 0 && !platinumMbr.isEmpty()) { // think carefully: what if there are multiple platinum members?
+//                pCount++;
+//                occupied = true;
+//                Event *pTmp = platinumMbr.removeFront();
+//                cout << "Answering call from " << pTmp->name << endl;
+//                platinumMbr.insertFront(pTmp); // should not use this for a queue
+//            }
+//            else if (gCount == 0 && !goldMbr.isEmpty()) {
+//                gCount++;
+//                occupied = true;
+//                Event *gTmp = goldMbr.removeFront();
+//                cout << "Answering call from " << gTmp->name << endl;
+//                goldMbr.insertFront(gTmp);
+//            }
+//            else if (sCount == 0 && !silverMbr.isEmpty()) {
+//                sCount++;
+//                occupied = true;
+//                Event *sTmp = silverMbr.removeFront();
+//                cout << "Answering call from " << sTmp->name << endl;
+//                goldMbr.insertFront(sTmp);
+//            }
+//            else if (rCount == 0 && !regularMbr.isEmpty()) {
+//                rCount++;
+//                occupied = true;
+//                Event *rTmp = regularMbr.removeFront();
+//                cout << "Answering call from " << rTmp->name << endl;
+//                regularMbr.insertFront(rTmp);
+//            }
+            if (!platinumMbr.isEmpty()) {
+                while (!platinumMbr.isEmpty()) {
+                    Event *pTmp = platinumMbr.removeFront();
+                    if (!occupied && pTmp->count == 0) {
+                        // flag of !occupied ensures operation on only the first object
+                        // circumvent the operation once flag is turned
+                        pTmp->count++;
+                        occupied = true;
+                        cout << "Answering call from " << pTmp->name << endl;
+                    }
+                    platinumMbr_tmp.insertBack(pTmp); // but this process can still be finished
+                }
+                platinumMbr = platinumMbr_tmp;
+                while (!platinumMbr_tmp.isEmpty()) {
+                    delete platinumMbr_tmp.removeFront();
+                }
             }
-            else if (gCount == 0 && !goldMbr.isEmpty()) {
-                gCount++;
-                is_answering = true;
-                Event *gTmp = goldMbr.removeFront();
-                cout << "Answering call from " << gTmp->name << endl;
-                goldMbr.insertFront(gTmp);
+            else if (!goldMbr.isEmpty()) {
+                while (!goldMbr.isEmpty()) {
+                    Event *gTmp = goldMbr.removeFront();
+                    if (!occupied && gTmp->count == 0) {
+                        gTmp->count++;
+                        occupied = true;
+                        cout << "Answering call from " << gTmp->name << endl;
+                    }
+                    goldMbr_tmp.insertBack(gTmp);
+                }
+                goldMbr = goldMbr_tmp;
+                while(!goldMbr_tmp.isEmpty()){
+                    delete goldMbr_tmp.removeFront();
+                }
             }
-            else if (sCount == 0 && !silverMbr.isEmpty()) {
-                sCount++;
-                is_answering = true;
-                Event *sTmp = silverMbr.removeFront();
-                cout << "Answering call from " << sTmp->name << endl;
-                goldMbr.insertFront(sTmp);
+            else if (!silverMbr.isEmpty()) {
+                while (!silverMbr.isEmpty()) {
+                    Event *sTmp = silverMbr.removeFront();
+                    if (!occupied && sTmp->count == 0) {
+                        sTmp->count++;
+                        occupied = true;
+                        cout << "Answering call from " << sTmp->name << endl;
+                    }
+                    silverMbr_tmp.insertBack(sTmp);
+                }
+                silverMbr = silverMbr_tmp;
+                while(!silverMbr_tmp.isEmpty()){
+                    delete silverMbr_tmp.removeFront();
+                }
             }
-            else if (rCount == 0 && !regularMbr.isEmpty()) {
-                rCount++;
-                is_answering = true;
-                Event *rTmp = regularMbr.removeFront();
-                cout << "Answering call from " << rTmp->name << endl;
-                regularMbr.insertFront(rTmp);
+            else if (!regularMbr.isEmpty()) {
+                while (!regularMbr.isEmpty()) {
+                    Event *rTmp = regularMbr.removeFront();
+                    if (!occupied && rTmp->count == 0) {
+                        rTmp->count++;
+                        occupied = true;
+                        cout << "Answering call from " << rTmp->name << endl;
+                    }
+                    regularMbr_tmp.insertBack(rTmp);
+                }
+                regularMbr = regularMbr_tmp;
+                while(!regularMbr_tmp.isEmpty()){
+                    delete regularMbr_tmp.removeFront();
+                }
             }
         }
         tick++;
